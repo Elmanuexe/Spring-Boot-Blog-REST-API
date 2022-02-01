e110e120a84e50fb3253a6b9661aa912eec96cb1
Alejandro Bajo <alebajodiaz@gmail.com>
2022-02-01 11:38:31 +0100

    Implementado el primer test de PhotoControllerTest

--- src/test/java/com/sopromadze/blogapi/controller/PhotoControllerTest.java
+++ src/test/java/com/sopromadze/blogapi/controller/PhotoControllerTest.java
@@ -7,6 +7,9 @@ import com.sopromadze.blogapi.payload.PagedResponse;
 import com.sopromadze.blogapi.payload.PhotoResponse;
 import com.sopromadze.blogapi.service.PhotoService;
 import lombok.extern.java.Log;
+import org.assertj.core.api.Assertions;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +20,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.List;
 
@@ -40,8 +44,6 @@ class PhotoControllerTest {
     void getAllPhotosTest() throws Exception{
 
         //Instanciamos todo lo necesario
-
-        Album album = new Album();
 
         PhotoResponse photo1 = new PhotoResponse(
                 1L,
@@ -67,19 +69,25 @@ class PhotoControllerTest {
                 1L
         );
 
+
         List<PhotoResponse> photos = List.of(photo1, photo2, photo3);
-        PagedResponse<PhotoResponse> pagedResponse = new PagedResponse<>(photos, 0, 1, 3, 1, true);
+        PagedResponse<PhotoResponse> pagedResponse = new PagedResponse<>(photos, 1, 1, 3, 1, true);
 
         //Mockeamos lo necesario
 
-        Mockito.when(photoService.getAllPhotos(0,1)).thenReturn(pagedResponse);
+        Mockito.when(photoService.getAllPhotos(1,1)).thenReturn(pagedResponse);
 
         //Implementamos el test
 
-        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/api/photos"))
-                .andExpect(status().isOk())
-                .andExpect(content().json(objectMapper.writeValueAsString(pagedResponse)))
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/api/photos")
+                        .param("page", "1")
+                        .param("size", "1"))
                 .andReturn();
+
+        String result = mvcResult.getResponse().getContentAsString();
+
+        Assertions.assertThat(result).isEqualToIgnoringWhitespace(objectMapper.writeValueAsString(pagedResponse));
+
     }
 
     @Test
