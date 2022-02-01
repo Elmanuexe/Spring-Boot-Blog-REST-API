4119a829240423996a6fb45cfd8a86da41cfb991
Alejandro Bajo <alebajodiaz@gmail.com>
2022-02-01 14:25:53 +0100

    Comenzado el segundo test, aun no va

--- src/test/java/com/sopromadze/blogapi/controller/PhotoControllerTest.java
+++ src/test/java/com/sopromadze/blogapi/controller/PhotoControllerTest.java
@@ -3,12 +3,14 @@ package com.sopromadze.blogapi.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sopromadze.blogapi.model.Album;
 import com.sopromadze.blogapi.model.Photo;
+import com.sopromadze.blogapi.model.role.RoleName;
 import com.sopromadze.blogapi.payload.PagedResponse;
+import com.sopromadze.blogapi.payload.PhotoRequest;
 import com.sopromadze.blogapi.payload.PhotoResponse;
+import com.sopromadze.blogapi.security.UserPrincipal;
 import com.sopromadze.blogapi.service.PhotoService;
 import lombok.extern.java.Log;
 import org.assertj.core.api.Assertions;
-import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -16,15 +18,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.PageImpl;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.Collections;
 import java.util.List;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+
 
 @Log
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = {SpringSecurityTestWebConfig.class})
@@ -41,7 +45,7 @@ class PhotoControllerTest {
     private PhotoService photoService;
 
     @Test
-    void getAllPhotosTest() throws Exception{
+    void getAllPhotosSuccessTest() throws Exception{
 
         //Instanciamos todo lo necesario
 
@@ -91,7 +95,48 @@ class PhotoControllerTest {
     }
 
     @Test
-    void addPhoto() {
+    @WithUserDetails("user")
+    void addPhotoSuccesTest() throws Exception{
+
+        //Instanciamos lo necesario
+
+        PhotoRequest request = new PhotoRequest();
+        request.setTitle("Photo 1");
+        request.setUrl("https://www.google.com");
+        request.setThumbnailUrl("https://www.google.com");
+
+        UserPrincipal user = new UserPrincipal(1L,
+                "user",
+                "user",
+                "user",
+                "user@gmail.com",
+                "user",
+                Collections.singleton(new SimpleGrantedAuthority(RoleName.ROLE_USER.toString())));
+
+        Album album = new Album();
+        album.setTitle("Vacaciones");
+
+        Photo photo = new Photo(
+                "Photo 1",
+                "https://www.google.com",
+                "https://www.google.com",
+                album
+        );
+        photo.setId(1L);
+
+        //Mockeamos lo necesario
+
+        Mockito.when(photoService.addPhoto(request, user)).thenReturn(photo);
+
+        //Implementamos el test
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/api/photos")
+                .content(objectMapper.writeValueAsString(request)))
+                .andReturn();
+
+        String result = mvcResult.getResponse().getContentAsString();
+        Assertions.assertThat(result).isEqualToIgnoringWhitespace(objectMapper.writeValueAsString(photo));
+
     }
 
     @Test
